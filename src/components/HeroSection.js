ab1c0641f0cbbb60dd2c412fb2aa4a736e8c0afc
rehannnnnn <hannn9491@gmail.com>
2021-11-12 15:15:46 +0700

    add new video di landing page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
     <div>
       <div className='hero-container'>
       
-      <video src='/videos/video-3.mp4' autoPlay loop muted />
+      <video src='./assets/video-hero.mp4' autoPlay loop muted />
       
     </div>
     </div>
