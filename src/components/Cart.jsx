f6b21c51fe09dc581e55f90eb6d589bce891b523
nurrahmiardani <73676678+nurrahmiardani@users.noreply.github.com>
2021-11-12 20:32:21 +0700

    cart dan navbar done

---
+++ src/components/Cart.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { ListGroup, Button, Row, Col, Image } from "react-bootstrap";
+import { CartState } from "../context/Context"
+
+export default function Cart () {
+    const {state : {cart}, dispatch} = CartState();
+
+    const [total,setTotal] = useState();
+
+    useEffect (() => {
+        setTotal(cart.reduce((acc, curr) => acc +Number(curr.Harga*curr.qty),0))
+    }, [cart] )
+
+    return (
+        <div className="home">
+            <div className="productCart">
+                <ListGroup>
+                    {cart.map(prod => (
+                        <ListGroup.Item key={prod.id}>
+                            <Row>
+                                <Col md={5}>
+                                    <Image style={{ height:"150px", width : "700px"}} src={prod.Img} alt={prod.Nama} fluid rounded/>
+                                </Col>
+                                <Col md={4}>
+                                    <span style={{fontWeight:"bold", fontSize: "20px"}}>{prod.Nama}</span> <br/>
+                                    <span style ={{fontSize : "18px"}}>{prod.Kota}</span> <br/>
+                                    <span style ={{fontSize : "18px"}}>{prod.Harga}.000</span> <br/> <br/>
+                                    <button className="btn-hapus" onClick = {()=> {
+                                        dispatch ({
+                                            type: 'REMOVE_FROM_CART',
+                                            payload: prod
+                                        })
+                                    }}>Hapus Dari Keranjang</button>
+                                   
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            </div>
+            <div className="filters-summary">
+                <form>
+                    <label>Nama Penerima</label><br/> 
+                    <input type ="text"></input><br/> <br/>
+                    <label>Alamat Penerima</label><br/> 
+                    <input type ="text"></input><br/> <br/>
+                    <label>Nomor Handphone Penerima</label><br/> 
+                    <input type ="number"></input> <br/> <br/>
+                </form>
+                <span>Total Pembelian Rp {total}.000</span> <br/><br/>
+                <span className='title'>Jumlah Produk ({cart.length}) buah</span> <br/><br/>
+                <button style= {{marginLeft:"200px", borderRadius: "5px"}}> 
+                    Beli Sekarang
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,124 @@
+import { Button, Dropdown, Badge } from "react-bootstrap";
+import {Link} from "react-router-dom"
 import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import { CartState } from "../context/Context";
+import {AiFillDelete} from "react-icons/ai";
+import {FaShoppingCart} from "react-icons/fa";
+import "../style/style.css"
+import "../style/Navbar.css"
 
-function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+export default function Header () {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    const {state: { cart }, dispatch} = CartState();
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+        setButton(false);
+        } else {
+        setButton(true);
+        }
+    };
 
-  useEffect(() => {
-    showButton();
-  }, []);
+    useEffect(() => {
+        showButton();
+    }, []);
 
-  window.addEventListener('resize', showButton);
+    window.addEventListener('resize', showButton);
 
-  return (
-    <>
-      <nav className='ininavbar'>
-        <div className='ininavbarcontainer'>
-          <Link to='/' className='ininavbarlogo' onClick={closeMobileMenu}>
-            Let's Sea
-          </Link>
-          <div className='inimenuicon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'ininavmenu active' : 'ininavmenu'}>
-            <li className='ininavitem'>
-              <Link to='/' className='ininavlinks' onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className='ininavitem'>
-              <Link
-                to='/edukasi'
-                className='ininavlinks'
-                onClick={closeMobileMenu}
-              >
-                Edukasi
-              </Link>
-            </li>
-            <li className='ininavitem'>
-              <Link
-                to='/transaksi'
-                className='ininavlinks'
-                onClick={closeMobileMenu}
-              >
-                Transaksi
-              </Link>
-            </li>
-            <li className='ininavitem'>
-              <Link
-                to='/about'
-                className='ininavlinks'
-                onClick={closeMobileMenu}
-              >
-                About
-              </Link>
-            </li>
-          </ul>
-          <button className="masuk">Sign Up</button>
-        </div>
-      </nav>
-    </>
-  );
+    return (
+        <>
+        <nav className='ininavbar'>
+            <div className='ininavbarcontainer'>
+            <Link to='/' className='ininavbarlogo' onClick={closeMobileMenu}>
+                Let's Sea
+            </Link>
+            <div className='inimenuicon' onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'ininavmenu active' : 'ininavmenu'}>
+                <li className='ininavitem'>
+                <Link to='/' className='ininavlinks' onClick={closeMobileMenu}>
+                    Home
+                </Link>
+                </li>
+                <li className='ininavitem'>
+                <Link
+                    to='/edukasi'
+                    className='ininavlinks'
+                    onClick={closeMobileMenu}
+                >
+                    Edukasi
+                </Link>
+                </li>
+                <li className='ininavitem'>
+                <Link
+                    to='/transaksi'
+                    className='ininavlinks'
+                    onClick={closeMobileMenu}
+                >
+                    Transaksi
+                </Link>
+                </li>
+                <li className='ininavitem'>
+                <Link
+                    to='/about'
+                    className='ininavlinks'
+                    onClick={closeMobileMenu}
+                >
+                    About
+                </Link>
+                </li>
+            </ul>
+            <Dropdown  style={{marginRight: "40px"}}>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    <FaShoppingCart/>
+                    <Badge>{cart.length}</Badge>
+                </Dropdown.Toggle >
+
+                <Dropdown.Menu style={{ minWidth: 350}}>
+                    {cart.length>0? (
+                         <>
+                            {cart.map((prod) => (
+                            <span className="cartitem" key={prod.id}>
+                                <img
+                                src={prod.Img}
+                                className="cartItemImg"
+                                alt={prod.Nama}
+                                />
+                                <div className="cartItemDetail">
+                                <span>{prod.Nama}</span>
+                                <span>Rp {prod.Harga}.000</span>
+                                </div>
+                                <AiFillDelete
+                                fontSize="20px"
+                                style={{ cursor: "pointer" }}
+                                onClick={() =>
+                                    dispatch({
+                                    type: "REMOVE_FROM_CART",
+                                    payload: prod,
+                                    })
+                                }
+                                />
+                            </span>
+                            ))}
+                            <Link to="/cart">
+                            <Button style={{ width: "95%", margin: "0 10px" }}>
+                                Go To Cart
+                            </Button>
+                            </Link>
+                        </>
+                    ): 
+                    (<span style= {{padding: 10}}> Cart is Empty</span>)}
+                    
+                </Dropdown.Menu>
+                </Dropdown>
+            <button className="masuk">Sign Up</button>
+            </div>
+        </nav>
+        </>
+    );
 }
-
-export default Navbar;
