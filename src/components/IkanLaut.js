ed383a932fd16b7af51672f1feab376ad08d586e
dindaviera <90680400+dindaviera@users.noreply.github.com>
2021-11-16 08:07:03 +0700

    kasih classname di file Menu.css

--- src/components/IkanLaut.js
+++ src/components/IkanLaut.js
@@ -18,19 +18,23 @@ function IkanLaut() {
 
     return (
         <div>
-            <h2>Nama-nama Ikan Laut</h2>
+            <div className="judul-list-ikan">
+                <h2>Nama-nama Ikan Laut</h2>
+            </div>
             <div className="item">
             {APIData.map(post =>
             (   <div className="item-con" key={post.id}>
                    <div className="item-container">
-                      <img src={post.Img} alt="" />
-                        <h4>{post.Nama}</h4>
-                        <h5>{post.NamaLatin}</h5>
-                        <p>
-                        Protein : {post.Protein} g <br/>
-                        Kalori : {post.Kalori} kkal <br/>
-                        Lemak : {post.Lemak} g <br/>
-                        </p>
+                    <img src={post.Img} alt="" />
+                        <div className = "nama-ikan">
+                            <h4>{post.Nama}</h4>
+                            <h5>{post.NamaLatin}</h5>
+                            <p>
+                            Protein : {post.Protein} g <br/>
+                            Kalori : {post.Kalori} kkal <br/>
+                            Lemak : {post.Lemak} g <br/>
+                            </p>
+                        </div>
                      </div>
                  </div>
             ))}
